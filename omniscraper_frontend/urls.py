704135b6199496022cfe9627a8d5a0f31a41f22a
Francis Mutiso <franckmutiso@gmail.com>
2021-05-29 19:51:18 +0300

    Add favicon to index.html

--- omniscraper_frontend/urls.py
+++ omniscraper_frontend/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, re_path
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 from . import views
 
 urlpatterns = [
     re_path(r'^(?:.*)/?$', views.index),
+    path('favicon.ico', RedirectView.as_view(
+        url=staticfiles_storage.url("favicon.ico"))),
 ]
