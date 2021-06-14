8f3a9aa1454ef0c256038c1fd55c5a209875233c
Francis Mutiso <franckmutiso@gmail.com>
2021-06-14 22:37:02 +0300

    Configure debug for production

--- omniscraper/settings.py
+++ omniscraper/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['omniscraper.herokuapp.com']
 
@@ -144,13 +144,13 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # enforce site-wide HTTPS
 
-# CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
-# SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
-# SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = True
 
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
