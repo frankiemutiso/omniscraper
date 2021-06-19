0a51059cec2185c74b4f8e2b8e15a2a63b24962a
Francis Mutiso <franckmutiso@gmail.com>
2021-06-19 20:14:13 +0300

    omniscraper-dev commits

--- omniscraper/settings.py
+++ omniscraper/settings.py
@@ -94,6 +94,9 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -184,6 +187,3 @@ SIMPLE_JWT = {
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
-
-db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES['default'].update(db_from_env)
