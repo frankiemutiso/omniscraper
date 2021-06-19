8143bd18f59305e7315ce7610629093ef553f2e0
Francis Mutiso <franckmutiso@gmail.com>
2021-06-19 19:21:44 +0300

    omniscraper-dev commits

--- omniscraper/settings.py
+++ omniscraper/settings.py
@@ -185,5 +185,5 @@ SIMPLE_JWT = {
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
-db_from_env = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
