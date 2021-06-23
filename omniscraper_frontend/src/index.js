299bbda95fa906f10ed6febc7b90b9d60fe04984
Francis Mutiso <franckmutiso@gmail.com>
2021-06-23 19:48:31 +0300

    Optimization attempts

--- omniscraper_frontend/src/index.js
+++ omniscraper_frontend/src/index.js
@@ -24,6 +24,13 @@ ReactDOM.render(
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js");
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("SW registered: ", registration);
+      })
+      .catch((err) => {
+        console.log("SW ERROR: ", err);
+      });
   });
 }
