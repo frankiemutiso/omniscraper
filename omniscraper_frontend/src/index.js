59c4b2a722cd7e160f439f945d6571107286327b
Francis Mutiso <franckmutiso@gmail.com>
2021-06-24 01:16:23 +0300

    Fixing bugs

--- omniscraper_frontend/src/index.js
+++ omniscraper_frontend/src/index.js
@@ -21,3 +21,16 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("app")
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./service-worker.js")
+      .then((registration) => {
+        console.log("SW registered: ", registration);
+      })
+      .catch((err) => {
+        console.log("SW ERROR: ", err);
+      });
+  });
+}
--- omniscraper_frontend/webpack.config.js
+++ omniscraper_frontend/webpack.config.js
@@ -2,6 +2,8 @@ const path = require("path");
 const BundleTracker = require("webpack-bundle-tracker");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const WorkboxPlugin = require("workbox-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = function (_env, argv) {
   const isProd = argv.mode === "production";
@@ -48,6 +50,18 @@ module.exports = function (_env, argv) {
             "css-loader",
           ],
         },
+        {
+          test: /\.module.css$/,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                modules: true,
+              },
+            },
+          ],
+        },
         { test: /\.worker\.js$/, loader: "worker-loader" },
       ],
     },
@@ -61,6 +75,25 @@ module.exports = function (_env, argv) {
       new WorkboxPlugin.GenerateSW(),
     ].filter(Boolean),
     optimization: {
+      minimize: isProd,
+      minimizer: [
+        new TerserWebpackPlugin({
+          terserOptions: {
+            compress: {
+              comparisons: false,
+            },
+            mangle: {
+              safari10: true,
+            },
+            output: {
+              comments: false,
+              ascii_only: true,
+            },
+            warnings: false,
+          },
+        }),
+        new OptimizeCssAssetsPlugin(),
+      ],
       usedExports: true,
       splitChunks: {
         chunks: "all",
