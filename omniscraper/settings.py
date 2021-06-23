b5c464c8a92622976775a76ccb8bdadfff8d8faa
Francis Mutiso <franckmutiso@gmail.com>
2021-06-24 00:10:42 +0300

    Debugging

--- omniscraper/settings.py
+++ omniscraper/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['omniscraper.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
