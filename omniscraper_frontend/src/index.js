51f7ffe5b4e8759c8161c966bad9744690b8012f
Francis Mutiso <franckmutiso@gmail.com>
2021-06-23 20:13:47 +0300

    Service worker config

--- omniscraper_frontend/src/index.js
+++ omniscraper_frontend/src/index.js
@@ -25,7 +25,7 @@ ReactDOM.render(
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/service-worker.js")
+      .register("./service-worker.js")
       .then((registration) => {
         console.log("SW registered: ", registration);
       })
