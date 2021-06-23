2a240185f37a9700e3d3c1025ca515e3104c4111
Francis Mutiso <franckmutiso@gmail.com>
2021-06-23 17:25:05 +0300

    Optimization attempts

--- omniscraper_frontend/webpack.config.js
+++ omniscraper_frontend/webpack.config.js
@@ -1,57 +1,89 @@
-const path = require("path")
-const BundleTracker = require('webpack-bundle-tracker')
-const TerserWebpackPlugin = require("terser-webpack-plugin")
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+const path = require("path");
+const BundleTracker = require("webpack-bundle-tracker");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: path.resolve(__dirname, './src/index.js'),
-  output: {
-    path: path.join(__dirname, './static/omniscraper_frontend/bundles'),
-    filename: '[name].js',
-    chunkFilename: "[id]-[chunkhash].js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+module.exports = function (_env, argv) {
+  const isProd = argv.mode === "production";
+  const isDev = !isProd;
+
+  return {
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "./static/omniscraper_frontend/bundles"),
+      filename: "[name].js",
+      chunkFilename: "[id]-[chunkhash].js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: true,
+              cacheCompression: false,
+              envName: isProd ? "production" : "development",
+            },
+          },
+        },
+        {
+          test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+          use: {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+            },
+          },
+        },
+        {
+          test: /\.(eot|otf|ttf|woff|woff2)$/,
+          loader: require.resolve("file-loader"),
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new BundleTracker({ filename: "./webpack-stats.json" }),
+      isProd &&
+        new MiniCssExtractPlugin({
+          filename:
+            "./static/omniscraper_frontend/bundles/[name].[contenthash:8].css",
+          chunkFilename:
+            "./static/omniscraper_frontend/bundles/[name].[contenthash:8].chunk.css",
+        }),
+    ].filter(Boolean),
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+        minSize: 0,
+        maxInitialRequests: 20,
+        maxAsyncRequests: 20,
+        cacheGroups: {
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module, chunks, cacheGroupKey) {
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )[1];
+              return `${cacheGroupKey}.${packageName.replace("@", "")}`;
+            },
+          },
+          common: {
+            minChunks: 2,
+            priority: -10,
+          },
         },
       },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        use: { loader: "url-loader" },
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new BundleTracker({filename: './webpack-stats.json'}),
-  ],
-  optimization: {
-    splitChunks: {
-      chunks: 'all', 
-      minSize: 0, 
-      maxInitialRequests: 20, 
-      maxAsyncRequests: 20,
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module, chunks, cacheGroupKey){
-            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-            return `${cacheGroupKey}.${packageName.replace("@", "")}`;
-          }
-        },
-        common: {
-          minChunks: 2, 
-          priority: -10
-        }
-      }
-    }, 
-    runtimeChunk: 'single'
-  }
+      runtimeChunk: "single",
+    },
+  };
 };
